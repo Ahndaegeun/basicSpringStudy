cb2fb66b96c4d6d8a40e5ecf0bf4ddc0f98985c7
ahndaegeun <adg0807@naver.com>
2022-03-15 21:15:49 +0900

    spring MVC converter and formatter end

---
+++ springMVC2-6/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,61 @@
+package hello.typeconverter.converter;
+
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConverterTest {
+
+    @Test
+    void stringToInteger() throws Exception {
+        //given
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+
+        //when
+        Integer result = converter.convert("10");
+
+        //then
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void integerToString() throws Exception {
+        //given
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+
+        //when
+        String result = converter.convert(10);
+
+        //then
+        assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void stringToIpPort() throws Exception {
+        //given
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+
+        //when
+        IpPort result = converter.convert(source);
+
+        //then
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+
+    @Test
+    void ipPortToString() throws Exception {
+        //given
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+
+        //when
+        String result = converter.convert(source);
+
+        //then
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+}
